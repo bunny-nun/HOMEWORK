cfd27bf3d733a4e0f660178badbbe1b156e879cf
Valeria <nelera.gb@gmail.com>
2022-11-12 17:47:26 +0300

    Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N

---
+++ 12.11.2022/Exercise08/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num01 = 1;
+while ( num01 <= num)
+{
+    if (num01%2 == 0) 
+    {
+        Console.Write(num01);
+        Console.Write(" ");
+    } 
+    num01++;
+}
