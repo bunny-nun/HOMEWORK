5d230811ad9dcc2253e3721b3d71ea58ed11f07d
Valeria <nelera.gb@gmail.com>
2022-11-13 16:57:47 +0300

    Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет, добавлена проверка отрицательных чисел

--- 13.11.2022/Exercise13/Program.cs
+++ 13.11.2022/Exercise13/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs (num);
 if (num < 100) Console.WriteLine("Третьей цифры нет");
 else
 {
