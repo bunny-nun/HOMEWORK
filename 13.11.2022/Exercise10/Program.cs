3b8950dc9b99222f51a253861602b234154b4ac4
Valeria <nelera.gb@gmail.com>
2022-11-13 16:53:11 +0300

    Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа, добавлена проверка отрицательных чисел

--- 13.11.2022/Exercise10/Program.cs
+++ 13.11.2022/Exercise10/Program.cs
@@ -2,6 +2,7 @@
 
 Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs (num);
 while (num < 100 || num > 999)
 {
     Console.WriteLine("Введено неверное число. Введите трехзначное число: ");
