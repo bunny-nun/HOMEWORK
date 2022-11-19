e5f0490d0e67988df31c8aafb7248947ba15896b
Valeria <nelera.gb@gmail.com>
2022-11-20 00:33:26 +0300

    Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ 20.11.2022/Exercise23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void square(double[] numbers)
+{
+    int index = 0;
+    int num = 1;
+    int length = numbers.Length;
+    while (num <= length)
+    {
+        numbers[index] = Math.Pow(num, 3);
+        Console.Write(numbers[index] + " ");
+        index++;
+        num++;
+    }
+}
+double[] array = new double[n];
+square(array);
