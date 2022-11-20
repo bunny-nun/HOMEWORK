8451067b34c372ff71c1ce01a2f732ec3594835a
Valeria <nelera.gb@gmail.com>
2022-11-20 14:22:11 +0300

    Упрощенная формула в комментарии

--- 13.11.2022/Exercise10/Program.cs
+++ 13.11.2022/Exercise10/Program.cs
@@ -8,4 +8,4 @@ while (num < 100 || num > 999)
     Console.WriteLine("Введено неверное число. Введите трехзначное число: ");
     num = Convert.ToInt32(Console.ReadLine());
 }
-Console.Write($"Вторая цифра введенного числа: {(num % 100 - num % 10) / 10}");
+Console.Write($"Вторая цифра введенного числа: {(num % 100 - num % 10) / 10}"); //num % 100 / 10
