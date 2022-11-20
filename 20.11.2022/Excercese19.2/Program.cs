5d14f4d9b3b36fb0178b198c620cb7a19bf39379
Valeria <nelera.gb@gmail.com>
2022-11-20 11:49:19 +0300

    Программа, которая принимает на вход любое число и проверяет, является ли оно палиндромом

---
+++ 20.11.2022/Excercese19.2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Программа, которая принимает на вход любое число и проверяет, является ли оно палиндромом
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num < 0)
+{
+    Console.WriteLine("Введено неверное число. Введите пятизначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+
+//while (num < 10000 || num > 99999)
+//{
+//    Console.WriteLine("Введено неверное число. Введите пятизначное число: ");
+//    num = Convert.ToInt32(Console.ReadLine());
+//}
+
+string newNumber(int number)
+{
+    string number01 = System.String.Empty;
+    while (number > 0)
+    {
+        number01 = number01 + (number % 10);
+        number = number / 10;
+    }
+    return number01;
+}
+int compNumber = Convert.ToInt32(newNumber(num));
+Console.WriteLine(compNumber);
+if (compNumber != num)
+{
+    Console.WriteLine($"Число {num} не является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {num} является палиндромом");
+}
