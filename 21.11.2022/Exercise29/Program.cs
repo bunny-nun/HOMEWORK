849bd652e9d17637343f8cf662b13f098fa546d8
Valeria <nelera.gb@gmail.com>
2022-11-21 17:52:52 +0300

    Программа, которая задаёт массив из 8 элементов и выводит их на экран

---
+++ 21.11.2022/Exercise29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Программа, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] Array(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+int[] newArray = Array(8);
+var a = string.Join (", ", newArray);
+Console.WriteLine($"[ {a} ]");
+
