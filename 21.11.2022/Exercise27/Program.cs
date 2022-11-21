2162230ee7c98f2f3b6e7ac6f50905de715a52ea
Valeria <nelera.gb@gmail.com>
2022-11-21 18:57:57 +0300

    Программа, которая принимает на вход число и выдаёт сумму цифр в числе

---
+++ 21.11.2022/Exercise27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Программа, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int Value(int a)
+{
+    int value = 0;
+    while (a != 0)
+    {
+        value += a % 10;
+        Console.Write(a % 10 + " ");
+        a /= 10;
+    }
+    Console.WriteLine(" ");
+    return value;
+}
+Console.WriteLine(Value(num));
