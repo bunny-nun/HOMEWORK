d0a705b7566d4f196955879720920cdf3588f873
Valeria <nelera.gb@gmail.com>
2022-11-26 22:35:23 +0300

    folder renamed

---
+++ 26.11.2022/Exercise34/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+int[] CreateArray(int length)
+{
+    int[] numbers = new int[length];
+    for (int n = 0; n < numbers.Length; n++)
+    {
+        numbers[n] = new Random().Next(100, 1000);
+        Console.Write(numbers[n] + " ");
+    }
+    Console.WriteLine();
+    return numbers;
+}
+
+Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+int sum = 0;
+for (int i = 0; i < length; i++)
+{
+    if ((array[i] % 2) == 0) sum++;
+}
+Console.WriteLine($"Количество четных чисел в массиве равно {sum}");
---
+++ 26.11.2022/Exercise36/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечетных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+
+int[] CreateArray(int length)
+{
+    int[] numbers = new int[length];
+    for (int n = 0; n < numbers.Length; n++)
+    {
+        numbers[n] = new Random().Next(0, 100);
+        Console.Write(numbers[n] + " ");
+    }
+    Console.WriteLine();
+    return numbers;
+}
+
+Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+int sum = 0;
+for (int i = 1; i < length; i += 2)
+{
+    sum += array[i];
+}
+Console.WriteLine($"Cумму элементов, стоящих на нечетных позициях {sum}");
