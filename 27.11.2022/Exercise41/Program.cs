5b4385b25cf201404a58ccb86f00c2993a5d620e
Valeria <nelera.gb@gmail.com>
2022-12-04 14:20:33 +0300

    Изменен ввод через строку

--- 27.11.2022/Exercise41/Program.cs
+++ 27.11.2022/Exercise41/Program.cs
@@ -2,6 +2,17 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3*/
 
+
+int[] ConvertToArray(string? input) // Конвертация string в int[]
+{
+    int[] numbers = input.Split(' ', '.', ',').Select(Int32.Parse).ToArray();
+    return numbers;
+}
+
+Console.WriteLine("Введите числа: ");
+string? inputNumbers = Console.ReadLine();
+
+/*
 int[] CreateArray()
 {
     Console.WriteLine("Введите количество чисел: ");
@@ -14,19 +25,19 @@ int[] CreateArray()
     }
     return newArray;
 }
+*/
 
-int[] array = CreateArray();
+int[] array = ConvertToArray(inputNumbers);
 
 int Count(int[] anyArray)
 {
     int count = 0;
-    for (int j = 0; j < anyArray.Length; j++)
+    for (int i = 0; i < anyArray.Length; i++)
     {
-        Console.Write(anyArray[j] + " ");
-        if (anyArray[j] > 0) count++;
+        Console.Write(anyArray[i] + " ");
+        if (anyArray[i] > 0) count++;
     }
     Console.WriteLine();
     return count;
 }
 Console.WriteLine($"Вы ввели {Count(array)} чисел больше 0");
-
